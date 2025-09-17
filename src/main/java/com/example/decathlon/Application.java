5c2090e757af060d4ee43c868afaa376b02c9c20
Staffan Åberg <staffan.aaberg@gmail.com>
2025-09-17 13:57:10 +0200

    Testar att lägga till en kommentar

--- src/main/java/com/example/decathlon/Application.java
+++ src/main/java/com/example/decathlon/Application.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
+    //Testar att lägga till en kommentar
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
