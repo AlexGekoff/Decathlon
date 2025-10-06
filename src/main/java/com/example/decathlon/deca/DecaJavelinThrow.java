23bb03a0bb946401720426e4c850b1fb142e1324
AlexGekoff <aleksandra.gekoff@utb.ecutbildning.se>
2025-10-06 10:20:15 +0200

    Rensat bort gränsvärdeskontroller från alla disciplin-klasser.Disciplinklasserna innehåller nu endast poängberäkning (beräkningslogik). Logiken för validering hanteras nu centralt i MainGUI och DecathlonService via ValidationUtil.

--- src/main/java/com/example/decathlon/deca/DecaJavelinThrow.java
+++ src/main/java/com/example/decathlon/deca/DecaJavelinThrow.java
@@ -9,34 +9,14 @@ public class DecaJavelinThrow {
 	private double A = 10.14;
 	private double B = 7;
 	private double C = 1.08;
-	boolean active = true;
+
 	CalcTrackAndField calc = new CalcTrackAndField();
-	InputResult inputResult = new InputResult();
 
 	// Calculate the score based on distance and height. Measured in meters.
 	public int calculateResult(double distance) {
 
-		while (active) {
+							score = calc.calculateField(A, B, C, distance);
 
-			try {
-				// Acceptable values.
-				if (distance < 0) {
-					System.out.println("Value too low");
-					distance = inputResult.enterResult();
-				} else if (distance > 110) {
-					System.out.println("Value too high");
-					distance = inputResult.enterResult();
-
-				} else {
-
-					score = calc.calculateField(A, B, C, distance);
-					active = false;
-				}
-			} catch (Exception e) {
-
-				System.out.println("Please enter numbers");
-			}
-		}
 		System.out.println("The result is: " + score);
 		return score;
 	}
